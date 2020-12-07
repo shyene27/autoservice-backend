b4a3475e4fe99dd741058e29e64e82615b4eb8f9
Sergiu Cretu - Moldova <SergiuCr@gilat.com>
2020-12-07 16:01:05 +0200

    initial commit

--- src/main/java/com/gideon/autoservice/config/ResourceServerConfig.java
+++ src/main/java/com/gideon/autoservice/config/ResourceServerConfig.java
@@ -24,10 +24,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        /*http.requestMatchers()
-                .antMatchers(SECURED_PATTERN).and().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SECURED_PATTERN).access(SECURED_WRITE_SCOPE)
-                .anyRequest().access(SECURED_READ_SCOPE);*/
+
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/users/{id}").hasAnyAuthority("ADMIN", "MECHANIC", "CUSTOMER")
                 .antMatchers(HttpMethod.GET, "/users/").hasAnyAuthority("ADMIN")
